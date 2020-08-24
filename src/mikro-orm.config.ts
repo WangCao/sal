10742728f1db0b8fcac86e41e96feb1000cf1d81
wangcao <345619747@qq.com>
2020-08-24 21:22:31 +0800

    add more info about post&user CURD

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
@@ -10,7 +11,7 @@ export default {
   },
   user: "mi",
   password: "123456",
-  entities: [Post],
+  entities: [Post, User],
   dbName: "sal",
   type: "postgresql",
   debug: !__prod__,
